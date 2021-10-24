bb49aab4f7aa874351c5a6eb3b8a1da42fb95c94
Aman <64945207+ashar-dev@users.noreply.github.com>
2021-10-24 16:57:06 +1100

    Update views.dart

--- views.dart
+++ views.dart
@@ -55,3 +55,48 @@
     );
   }
 
+# Card Building Method fpr MyHomePageState
+Card buildCard() {
+    var heading = '\$2300 per month';
+    var subheading = '2 bed, 1 bath, 1300 sqft';
+    var cardImage = NetworkImage(
+        'https://source.unsplash.com/random/800x600?house');
+    var supportingText =
+        'Beautiful home to rent, recently refurbished with modern appliances...';
+    return Card(
+        elevation: 4.0,
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(heading),
+              subtitle: Text(subheading),
+              trailing: Icon(Icons.favorite_outline),
+            ),
+            Container(
+              height: 200.0,
+              child: Ink.image(
+                image: cardImage,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(16.0),
+              alignment: Alignment.centerLeft,
+              child: Text(supportingText),
+            ),
+            ButtonBar(
+              children: [
+                TextButton(
+                  child: const Text('CONTACT AGENT'),
+                  onPressed: () {/* ... */},
+                ),
+                TextButton(
+                  child: const Text('LEARN MORE'),
+                  onPressed: () {/* ... */},
+                )
+              ],
+            )
+          ],
+        ));
+  }
+
